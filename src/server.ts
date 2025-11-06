26a905ff27ccf20405daa6ccdbd30d987ed544e3
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-06 15:14:08 +0200

    feat: created the projectRoutes, & checkAuth files

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,24 @@
+// import express, {Request, Response, application } from "express";
+
+
+// require('dotenv').config();
+
+// const express = require('express')
+// const app = express()
+
+// const PORT = process.env.PORT || 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(PORT, () => {
+//   console.log(`Example app listening on port ${PORT}`)
+// })
+
+
+// startServer();
+
 import express, { application } from "express";
 import dotenv from "dotenv";
 import { log } from "console";
@@ -16,9 +37,11 @@ const startServer = async () => {
   app.use('/api/auth', authRoutes);
   app.use('/api',applicationRoutes);
 
-  app.use('/api', applicationRoutes);
+  app.use('/api', applicationRoutes); // add request, response and next parameters and an error handling middleware. import Next:NextFunction from express..
+  // //res.status(404).json({message: 'Not Found,Internal Server Error', success: false});
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
 };
+
 startServer();
