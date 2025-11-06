bb1363d2882201422c76a3c8047385174dfb06dc
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-11-06 16:44:56 +0200

    feat: created controllers, routes, & public folders

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 // import express, {Request, Response, application } from "express";
 
-
 // require('dotenv').config();
 
 // const express = require('express')
@@ -9,17 +8,17 @@
 // const PORT = process.env.PORT || 3000
 
 // app.get('/', (req, res) => {
-//   res.send('Hello World!')
+//   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 // })
 
 // app.listen(PORT, () => {
 //   console.log(`Example app listening on port ${PORT}`)
 // })
 
-
 // startServer();
 
 import express, { application } from "express";
+import path from "path";
 import dotenv from "dotenv";
 import { log } from "console";
 import { testDBConnection } from "./config/database";
@@ -34,10 +33,16 @@ const PORT = process.env.APP_PORT || 3000;
 const startServer = async () => {
   await testDBConnection();
   app.use(express.json());
-  app.use('/api/auth', authRoutes);
-  app.use('/api',applicationRoutes);
 
-  app.use('/api', applicationRoutes); // add request, response and next parameters and an error handling middleware. import Next:NextFunction from express..
+  //serve static files
+  app.use(express.static(path.join(__dirname, "public"))); //the public folder is the statis folder which contains all the static files like css, js , images etc
+  app.use("/api/auth", authRoutes);
+  app.use("/api", applicationRoutes);
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+  });
+
+  app.use("/api", applicationRoutes); // add request, response and next parameters and an error handling middleware. import Next:NextFunction from express..
   // //res.status(404).json({message: 'Not Found,Internal Server Error', success: false});
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
